41d81e9ffcd682f83a3a0eb8a888d43532f19c92
KaladaJumbo <wkalada1@gmail.com>
2020-10-10 18:22:12 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    
+    drivers.push(name);
+    return drivers
+
+}
+
+function destructivelyPrependDriver(name) {
+    
+    drivers.unshift(name);
+    return drivers
+
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+    return drivers
+}
+
+function destructivelyRemoveFirstDriver(params) {
+
+    drivers.shift();
+    return drivers
+    
+}
+
+function appendDriver(name) {
+    
+    let new_array = [...drivers]
+    new_array.push(name)
+    return new_array
+
+}
+
+function prependDriver(name) {
+
+    let new_array = [...drivers]
+    new_array.unshift(name)
+    return new_array
+    
+}
+
+function removeLastDriver() {
+
+    let new_array = [...drivers]
+    new_array.pop()
+    return new_array
+    
+}
+
+function removeFirstDriver() {
+
+    let new_array = [...drivers]
+    new_array.shift()
+    return new_array
+    
+    
+}
